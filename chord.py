260f42dd42a375237f3f5033f766a1750e235a40
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-02 20:02:11 +0330

    def addId

--- chord.py
+++ chord.py
@@ -1,6 +1,7 @@
 import random
 
 data_keys = []
+node_ids = []
 class Data:
     def __init__(self,v):
         self.val = v
@@ -10,7 +11,6 @@ class Data:
         val_key = random.randint(1,32)
         while(val_key in data_keys):
             val_key = random.randint(1,32)
-        
         data_keys.append(val_key)
         return val_key
 
@@ -21,3 +21,10 @@ class Node:
         self.succ = None
         self.datas = []
         self.ft = []
+
+    def addId(self):
+        val_id = random.randint(1,32)
+        while(val_id in node_ids):
+            val_id = random.randint(1,32)
+        node_ids.append(val_id)
+        return val_id
