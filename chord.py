cf7da06a47360e45538185043b08629a70347861
hrakbari77@gmail.com <hrakbari77@gmail.com>
2021-02-02 19:45:36 +0330

    def addkey added

--- chord.py
+++ chord.py
@@ -1,11 +1,20 @@
 import random
 
+
+data_keys = []
 class Data:
     def __init__(self,v):
         self.val = v
-        self.key = random.randrange(1, 32)
+        self.key = self.addKey()
     
+    def addKey(self):
+
+        self.key = random.randint(1,32)
+        while(self.key in data_keys):
+            self.key = random.randint(1,32)
     
+        data_keys.append(self.key)
+
 
 class Node:
     def __init__(self):
